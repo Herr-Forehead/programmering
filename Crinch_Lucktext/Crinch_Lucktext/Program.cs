9a79d96249cf0756a18d276b9a3c1d863761d888
Leonard Alm <leonard.alm@elev.ga.ntig.se>
2022-08-31 14:22:29 +0200

    Initial commit

---
+++ Crinch_Lucktext/Crinch_Lucktext/Program.cs
@@ -0,0 +1,24 @@
+﻿//Console.WriteLine("Hej Nti");
+//Console.WriteLine("Hej Te21B");
+
+string name;
+string food;
+string fSport;
+string iSport;
+
+Console.WriteLine("Vad heter du?");
+name = Console.ReadLine();
+
+Console.WriteLine("Vad för mat ogillar du?");
+food = Console.ReadLine();
+
+Console.WriteLine("Vad är din favorit sport?");
+fSport = Console.ReadLine();
+
+Console.WriteLine("vilken sport gillar du inte?");
+iSport = Console.ReadLine();
+
+Console.WriteLine($"Hej {name}! du har precis vunnit en biljett det bästa hotellet någonsin, där {food} serveras varje dag, {fSport} är olagligt och {iSport} är obligatoriskt. Ses snart~! (ps. Du kan också kolla på Morbius varje dag)");
+
+
+Console.ReadLine();
