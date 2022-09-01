9d3aa3381763b63984a187a95fdf9c6344494959
Leonard Alm <leonard.alm@elev.ga.ntig.se>
2022-09-01 22:09:50 +0200

    attempt1

--- null/story_tajm/story_tajm/Program.cs
+++ null/story_tajm/story_tajm/Program.cs
@@ -1,1 +1,30 @@
 ﻿
+Console.WriteLine("You wake up in a verii scary forest and you see a lone path ahead, you can choose to walk it or not, what do?(yes/no)");
+string choice1 = Console.ReadLine();
+if(choice1 == "yes")
+{
+    Console.WriteLine("you walk further into the forest and find a lake in a sword, do you take it?(yes/no)");
+    string choice2 = Console.ReadLine();
+    if(choice2 == "yes")
+    {
+        Console.WriteLine("congrats you got a lake!");
+    }
+    else if(choice2 == "cum")
+    {
+        Console.WriteLine("The cum monster killed you!");
+    }
+    else
+    {
+        Console.WriteLine("You didn't get lake but could look at the sword.");
+    }
+}
+else if(choice1 == "cum")
+{
+    Console.WriteLine("the cum monster killed you!");
+}
+else
+{
+    Console.WriteLine("you eventually fell asleep");
+}
+
+Console.ReadLine();
