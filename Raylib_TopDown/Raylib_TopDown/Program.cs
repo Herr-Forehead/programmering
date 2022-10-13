bf276eab85cd65de2766d571d9d6ebf9d515be64
Leonard Alm <leonard.alm@elev.ga.ntig.se>
2022-10-13 15:01:57 +0200

    first commit

---
+++ Raylib_TopDown/Raylib_TopDown/Program.cs
@@ -0,0 +1,43 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "Leonards crinch spel");
+Raylib.SetTargetFPS(60);
+
+Color royalBlue = new Color(85, 118, 209, 255);
+
+Rectangle playerRect = new Rectangle(368, 268, 64, 64);
+
+float speed = 5;
+
+
+while (Raylib.WindowShouldClose() == false)
+{
+    // Logik
+
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+    {
+        playerRect.x += speed;
+    }
+    else if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+    {
+        playerRect.x -= speed;
+    }
+    else if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+    {
+        playerRect.y -= speed;
+    }
+    else if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+    {
+        playerRect.y += speed;
+    }
+
+
+    // Grafik
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.DARKPURPLE);
+
+Raylib.DrawRectangleRec(playerRect, royalBlue);
+    // Raylib.DrawRectangle(384, 284, 32, 32, royalBlue);
+
+    Raylib.EndDrawing();
+}
