7ef82528759fa6da5fb0dbb5623dbc478076a3e2
Leonard Alm <leonard.alm@elev.ga.ntig.se>
2022-10-20 16:10:43 +0200

    hitbox och background

--- Raylib_TopDown/Raylib_TopDown/Program.cs
+++ Raylib_TopDown/Raylib_TopDown/Program.cs
@@ -6,16 +6,23 @@ Raylib.SetTargetFPS(60);
 Color royalBlue = new Color(85, 118, 209, 255);
 Color transparent = new Color(255, 255, 255, 0);
 
-Rectangle playerRect = new Rectangle(368, 268, 64, 64);
-Texture2D avatarImage = Raylib.LoadTexture("test_character.png");
+Texture2D avatarImage = Raylib.LoadTexture("stolencharacteravatar.png");
+Texture2D bkgImage = Raylib.LoadTexture("actual_background.png");
 
-float speed = 5;
+Rectangle playerRect = new Rectangle(368, 268, avatarImage.width, avatarImage.height);
+Rectangle trapRect = new Rectangle (700, 500, 64, 64);
+
+float speed = 5.5f;
+
+string currentScene = "start"; // start, game, end
 
 
 while (Raylib.WindowShouldClose() == false)
 {
     // Logik
 
+    if (currentScene == "game")
+    {
     if (Raylib.IsKeyDown(KeyboardKey.KEY_D) || Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
     {
         playerRect.x += speed;
@@ -33,15 +40,44 @@ while (Raylib.WindowShouldClose() == false)
         playerRect.y += speed;
     }
 
+    if (Raylib.CheckCollisionRecs(playerRect, trapRect))
+    {
+        currentScene = "end";
+    }
+    }
+    else if (currentScene == "start")
+    {
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
+        {
+            currentScene = "game";
+        }
+    }
 
     // Grafik
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.DARKPURPLE);
 
-    Raylib.DrawTexture(avatarImage,
-     (int)playerRect.x, 
-     (int)playerRect.y, 
-     Color.WHITE);
+    if (currentScene == "game")
+    {
+     Raylib.DrawTexture(bkgImage, 0, 0, Color.WHITE);
+
+     Raylib.DrawTexture(avatarImage,
+          (int)playerRect.x, 
+          (int)playerRect.y, 
+          Color.WHITE);
+
+     Raylib.DrawRectangleRec(trapRect, Color.RED);
+    }
+    else if (currentScene == "start")
+    {
+
+        Raylib.DrawText("press ENTER to start", 400, 300, 30, Color.GREEN);
+    }
+    else if (currentScene == "end")
+    {
+        Raylib.DrawText("you lost?, how crinch", 350, 250, 30, Color.GREEN);
+    }
+
 
     // Raylib.DrawRectangleRec(playerRect, royalBlue);
     // Raylib.DrawRectangle(384, 284, 32, 32, royalBlue);
