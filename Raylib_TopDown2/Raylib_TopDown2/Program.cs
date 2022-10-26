f19104e20297bc5e12e43902864a155fbd138338
Leonard Alm <leonard.alm@elev.ga.ntig.se>
2022-10-26 14:37:51 +0200

    lists added and octosniper

--- Raylib_TopDown2/Raylib_TopDown2/Program.cs
+++ Raylib_TopDown2/Raylib_TopDown2/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "Leonards even more crinch game");
+Raylib.SetTargetFPS(60);
+
+Texture2D enemyTexture = Raylib.LoadTexture("octosnipah.png");
+// Rectangle enemyRect = new Rectangle(
+//   0,  0, 
+//   enemyTexture.width, enemyTexture.height
+//   );
+
+List<Rectangle> enemyRects = new List<Rectangle>();
+
+enemyRects.Add(new Rectangle(0, 0, enemyTexture.width, enemyTexture.height));
+enemyRects.Add(new Rectangle(300, 0, enemyTexture.width, enemyTexture.height));
+enemyRects.Add(new Rectangle(700, 0, enemyTexture.width, enemyTexture.height));
+
+
+while(!Raylib.WindowShouldClose())
+{
+    // LOGIK
+
+    // GRAFIK
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.DARKBLUE);
+
+    Raylib.DrawTexture(enemyTexture, 
+      (int)enemyRects[0].x,
+      (int)enemyRects[0].y,
+      Color.WHITE
+    );
+
+    Raylib.EndDrawing();
+}
