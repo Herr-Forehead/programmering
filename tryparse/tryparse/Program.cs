227f46f8bea1c22771eab7af9f105d1958583af9
Leonard Alm <leonard.alm@elev.ga.ntig.se>
2022-11-10 09:34:13 +0100

    initial commit

---
+++ tryparse/tryparse/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+List<string> names = new List<string>();
+
+names.Add("Alex");
+
+
+
+Console.ReadLine();
+
+// int num = 0;
+// bool success = false;
+
+// while (!success)
+// {
+//   Console.WriteLine("skriv siffra");
+//   string numText = Console.ReadLine();
+
+//   success = int.TryParse(numText, out num);
+
+//   if (!success)
+//   {
+//     Console.WriteLine("waaw, u r have stupid");
+//   }
+// }
+
+// Console.WriteLine(num);
+
+// Console.ReadLine();
