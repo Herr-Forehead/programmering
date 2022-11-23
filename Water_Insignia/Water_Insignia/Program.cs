11d2c5cb868a83c3bc90086927eaec00f522a858
Leonard Alm <leonard.alm@elev.ga.ntig.se>
2022-11-23 14:47:59 +0100

    avatar added

--- Water_Insignia/Water_Insignia/Program.cs
+++ Water_Insignia/Water_Insignia/Program.cs
@@ -1,1 +1,32 @@
 ﻿using Raylib_cs;
+
+Raylib.InitWindow(600, 800, "Water Insignia");
+Raylib.SetTargetFPS(60);
+
+Texture2D AvatarImage = Raylib.LoadTexture("gremory.png");
+int tileSize = 32;
+
+while (!Raylib.WindowShouldClose())
+{
+    // Logik
+
+
+
+    // Grafik
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.WHITE);
+
+    for (int x = 0; x < Raylib.GetScreenWidth() / tileSize; x++)
+    {
+        Raylib.DrawLine(x * tileSize, 0, x * tileSize, Raylib.GetScreenHeight(), Color.BLACK);
+    }
+    for (int y = 0; y < Raylib.GetScreenHeight() / tileSize; y++)
+    {
+        Raylib.DrawLine(Raylib.GetScreenWidth(), y * tileSize, 0, y * tileSize, Color.BLACK);
+    }
+
+    Raylib.DrawTexture(AvatarImage, 0, 32, Color.BLACK);
+
+    // hielo
+    Raylib.EndDrawing();
+}
