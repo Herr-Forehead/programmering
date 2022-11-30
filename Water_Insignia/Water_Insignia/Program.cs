03e8168de6cf03517bbea51bd75ff54cb03443dd
Leonard Alm <leonard.alm@elev.ga.ntig.se>
2022-11-30 14:24:44 +0100

    movement added (I think)

--- Water_Insignia/Water_Insignia/Program.cs
+++ Water_Insignia/Water_Insignia/Program.cs
@@ -9,7 +9,7 @@
 // add music
 
 // if I get this far, try to add enemies and add turns
-// different weapon types/classes
+// different weapon types and character classes
 // add weapon durability
 // add stats and combat
 // feebly attempt to make somewhat of an enemy AI
@@ -18,6 +18,7 @@ Raylib.InitWindow(600, 800, "Water Insignia");
 Raylib.SetTargetFPS(60);
 
 Texture2D AvatarImage = Raylib.LoadTexture("gremory.png");
+Rectangle AvatarRect = new Rectangle(0, 0, AvatarImage.width, AvatarImage.height);
 int tileSize = 32;
 
 while (!Raylib.WindowShouldClose())
@@ -25,11 +26,43 @@ while (!Raylib.WindowShouldClose())
     // Logik
     int infantryMov = 3;
 
+    while (infantryMov > 0 || infantryMov == 0)
+    {
+
+        if (Raylib.IsKeyReleased(KeyboardKey.KEY_RIGHT))
+        {
+            AvatarRect.x += 32;
+            infantryMov -= 1;
+            if (Raylib.IsKeyReleased(KeyboardKey.KEY_LEFT))
+            {
+                AvatarRect.x -= 32;
+                infantryMov += 1;
+            }
+        }
+        else if (Raylib.IsKeyReleased(KeyboardKey.KEY_LEFT))
+        {
+
+            AvatarRect.x -= 32;
+            infantryMov -= 1;
+        }
+        else if (Raylib.IsKeyReleased(KeyboardKey.KEY_UP))
+        {
+            AvatarRect.y += 32;
+            infantryMov -= 1;
+        }
+        else if (Raylib.IsKeyReleased(KeyboardKey.KEY_DOWN))
+        {
+            AvatarRect.y -= 32;
+            infantryMov -= 1;
+        }
+
+    }
+
 
     // Grafik
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.WHITE);
-    Raylib.DrawTexture(AvatarImage, 0, 0, Color.WHITE);
+    Raylib.DrawTexture(AvatarImage, (int)AvatarRect.x, (int)AvatarRect.y, Color.WHITE);
 
     for (int x = 0; x < Raylib.GetScreenWidth() + 1 / tileSize; x++)
     {
