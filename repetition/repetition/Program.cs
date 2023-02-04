3b29ccdbc859ccbea8bb16d346318d781cec21be
Leonard Alm <leonard.alm@elev.ga.ntig.se>
2023-02-04 21:16:07 +0100

    repetition

---
+++ repetition/repetition/Program.cs
@@ -0,0 +1,48 @@
+﻿// Arrayer och listor
+
+string name = "Leonard";
+
+string[] frens = {"Alex", "Dhani", "Billiam" };
+
+List<string> weapons = new List<string>();
+weapons.Add("Frozen Naraka");
+weapons.Add("Shuhadaku of Uriel");
+weapons.Add("Ruinous Sakura");
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine(i);
+}
+
+// frens[4] = "Hernando";
+
+// Test(frens);
+// Console.WriteLine(frens[0]);
+
+// Test2(name);
+// Console.WriteLine(name);
+
+// static void Test(string[] stuff)
+// {
+//     stuff[0] = "Josefin";
+// }
+
+// static void Test2(string[] stuff)
+// {
+//     stuff = "hej";
+// }
+
+
+// Console.WriteLine(frens[0]);
+
+// int[,] map = { 
+//     {9, 10, 11}, 
+//     {12, 13, 14}
+// };
+
+// int[,,,] cube;
+
+// Console.WriteLine(map[0,2]);
+
+
+Console.ReadLine();
